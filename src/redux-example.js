0ef1889e8b40aeb32430dc598ce1bd8b2d2315a9
thelittleblacksmith <einers@outlook.com>
2017-04-03 21:49:12 -0700

    dispatch and handling actions

--- src/redux-example.js
+++ src/redux-example.js
@@ -5,12 +5,29 @@ import Redux, { createStore } from 'redux';
 console.log('starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-  return state;
+  switch(action.type){
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      } 
+    default:
+      return state
+  }
 }
 
 var store = createStore(reducer);
 
 //gets our current object
 var currentState = store.getState();
-
 console.log('currentState', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
+}
+
+//dispatch your action
+store.dispatch(action);
+
+console.log('Name should be andrew', store.getState())
--- src/redux-todo-example.js
+++ src/redux-todo-example.js
@@ -9,10 +9,29 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
-  return state;
+  switch(action.type){
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+    default:
+      return state;
+  }
 }
 
 var store = createStore(reducer);
 
 //console.log the results 
 console.log('currentState', store.getState());
+
+var changeSearchText = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'penguins'
+}
+
+//dispatch the action
+
+store.dispatch(changeSearchText);
+
+console.log('search text should be penguins', store.getState())
