d4f5b685b84b21c9b264ace12abe6402d2821c13
thelittleblacksmith <einers@outlook.com>
2017-04-03 23:06:20 -0700

    reducer compositions yay

--- src/redux-example.js
+++ src/redux-example.js
@@ -1,6 +1,6 @@
 //Simple redux app
 
-import Redux, { createStore, compose } from 'redux';
+import Redux, { createStore, compose, combineReducers } from 'redux';
 
 console.log('starting redux example');
 
@@ -13,7 +13,7 @@ var stateDefault = {
 var nextHobbyId = 1;
 var nextMovieId = 1;
 
-var reducer = (state = stateDefault, action) => {
+var oldReducer = (state = stateDefault, action) => {
   switch(action.type){
     case 'CHANGE_NAME':
       return {
@@ -65,6 +65,60 @@ var reducer = (state = stateDefault, action) => {
   }
 }
 
+var nameReducer = (state = 'Anonymous', action) => {
+  switch(action.type){
+    case 'CHANGE_NAME':
+      //state is no longer an object so you only return the changes
+      return action.name
+    default: 
+      return state;
+  }
+}
+
+var hobbiesReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_HOBBY':
+      return [
+        ...state,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ];
+    case 'REMOVE_HOBBY':
+      //filter will return a new array
+      return state.filter((hobby) => { hobby.id !== action.id})
+    default: 
+      return state;
+  }
+}
+
+var movieReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_MOVIE':
+      return [
+        ...state,
+        {
+          id: nextMovieId++,
+          title: action.title,
+          genre: action.genre
+        }
+      ]
+    case 'REMOVE_MOVIE':
+      return state.filter((movie) => { movie.id !== action.id })
+    default: 
+      return state;
+  }
+}
+
+//reducer composition
+var reducer = combineReducers({
+  //name state is goign to get managed by nameReducer
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: movieReducer
+})
+
 var store = createStore(reducer, compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
@@ -108,8 +162,19 @@ store.dispatch({
 })
 
 store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Beauty and the Beast',
+  genre: 'Adaptation'
+});
+
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Knitting'
 });
 
 store.dispatch({
